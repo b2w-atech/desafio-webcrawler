88a9b29700590bed58a32cb6f0ba8860f502c38d
luanascardua <92794401+luanascardua@users.noreply.github.com>
2022-08-03 07:03:16 -0300

    style: queries

--- webcrawler/webcrawler/queries/queries.js
+++ webcrawler/webcrawler/queries/queries.js
@@ -1,10 +1,10 @@
-// citações foram coletadas
+// citações coletadas
 dbo.luana_scardua.count()
 
 // tags distintas
 db.luana_scardua.distinct("tags")
 
-// citações por autor foram
+// citações por autor
 db.luana_scardua.aggregate(
     [{
         $group: {_id: "$author.name", qtd: {$sum: 1}}
